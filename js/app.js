580eff3699bbc94d46b6b37edbc9932e9dfc5c23
golibov <golibovshg7@gmail.com>
2024-08-18 00:20:06 +0500

    plus: modal,dark-mode,design

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 "use strict";
 
-
 let list = document.querySelector(".list");
 let search = document.querySelector(".search");
 search.className = 'search w-[300px] h-[60px] border-[1px] rounded-lg ';
+
 const countrys = [
     {
         id: 1,
@@ -75,45 +75,52 @@ const countrys = [
         population: 786559,
         flag: "https://flagcdn.com/gy.svg"
     }
-]
+];
 
 function render(arr) {
     list.innerHTML = null;
-    arr.map((item) => {
+    arr.forEach((item) => {
         let userItem = document.createElement("li");
         userItem.classList.add("user-item");
-        let wrapper = document.createElement("div")
-        wrapper.classList.add("wrapper")
+        let wrapper = document.createElement("div");
+        wrapper.classList.add("wrapper");
 
-        let heartIcon = document.createElement('img')
-        heartIcon.className = 'w-[35px] h-[35px] heart'
-        heartIcon.src = '../images/heart-1.svg'
+        let heartIcon = document.createElement('img');
+        heartIcon.className = 'w-[35px] h-[35px] heart';
+        heartIcon.src = '../images/heart-1.svg';
 
-        let saveIcon = document.createElement('img')
-        saveIcon.className = 'w-[30px] h-[30px]'
-        saveIcon.src = '../images/save-3.svg'
+        let saveIcon = document.createElement('img');
+        saveIcon.className = 'w-[30px] h-[30px]';
+        saveIcon.src = '../images/save-3.svg';
 
-        let deleteIcon = document.createElement('img')
-        deleteIcon.className = 'w-[30px] h-[30px] delete-icon'
-        deleteIcon.src = '../images/delete.svg'
+        let deleteIcon = document.createElement('img');
+        deleteIcon.className = 'w-[30px] h-[30px] delete-icon';
+        deleteIcon.src = '../images/delete.svg';
+
+        let more = document.createElement('h3');
+        more.className = 'text-[15px] text-[white] border-[1px] rounded-lg p-1 more';
+        more.textContent = 'more';
 
         userItem.innerHTML = `
-            <img class=" flag w-[100px] h-[100px]" src=${item.flag} alt="flag" width="500px">
-            <h1> name: ${item.name}</h1>
-            <p>capital:${item.capital}</p>
-            <p> population:${item.population}</p>
-            <p> id:${item.id}</p>
-        `
+            <img class="flag w-[100px] h-[100px]" src=${item.flag} alt="flag">
+            <h1>name: ${item.name}</h1>
+            <p>capital: ${item.capital}</p>
+            <p>population: ${item.population}</p>
+            <p>id: ${item.id}</p>
+        `;
 
         list.appendChild(userItem);
         userItem.appendChild(wrapper);
-        wrapper.append(heartIcon, saveIcon, deleteIcon);
-
+        wrapper.append(heartIcon, saveIcon, deleteIcon, more);
 
         deleteIcon.addEventListener('click', function () {
             userItem.remove();
         });
-    })
+
+        more.addEventListener('click', function () {
+            openModal(item);
+        });
+    });
 }
 
 render(countrys);
@@ -125,8 +132,40 @@ search.addEventListener("keyup", function (evt) {
 });
 
 
+const modal = document.createElement('div');
+modal.className = 'modal fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center hidden';
+modal.innerHTML = `
+    <div class="modal-content  p-4 rounded-lg w-[500px] h-[300px] flex flex-col items-center justify-center backdrop-blur content-auto">
+        <button class="close-button"><img src="../images/x-lg.svg" alt=""></button>
+        <h2 class="modal-title text-lg font-bold"></h2>
+        <img class="modal-flag  w-[100px] h-[100px]" src="" alt="flag" >
+        <p class="modal-capital"></p>
+        <p class="modal-population"></p>
+        <p class="modal-id"></p>
+    </div>
+`;
 
+document.body.appendChild(modal);
 
+function openModal(country) {
+    modal.querySelector('.modal-title').textContent = country.name;
+    modal.querySelector('.modal-flag').src = country.flag;
+    modal.querySelector('.modal-capital').textContent = `Capital: ${country.capital}`;
+    modal.querySelector('.modal-population').textContent = `Population: ${country.population}`;
+    modal.querySelector('.modal-id').textContent = `ID: ${country.id}`;
+
+    modal.classList.remove('hidden');
+}
+
+modal.querySelector('.close-button').addEventListener('click', function () {
+    modal.classList.add('hidden');
+});
+
+window.addEventListener('click', function (e) {
+    if (e.target === modal) {
+        modal.classList.add('hidden');
+    }
+});
 
 const darkButton = document.querySelector('.dark-body');
 darkButton.addEventListener('click', () => {
@@ -134,6 +173,7 @@ darkButton.addEventListener('click', () => {
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundRepeat = 'no-repeat';
     document.body.style.backgroundPosition = 'center';
-
-
 });
+
+
+
